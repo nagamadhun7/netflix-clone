851b1de186f280572c0804795d06426dfa1af039
nagamadhun7 <nadakudurunagamadhu@gmail.com>
2025-01-15 12:24:03 -0500

    Render Banner in HomeScreen

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import '../styles/homeScreen.css'
 import Navbar from './Navbar'
+import Banner from './Banner'
 
 const HomeScreen = () => {
   return (
     <div className='homeScreen'>
         <Navbar />
+        <Banner />
     </div>
   )
 }
